cfd5dfb2f7b01f2bdef60c0ca0aaabb184e6876e
arbazshabhai <49944243+arbazshabhai@users.noreply.github.com>
2019-09-07 01:07:11 +0530

    Add project files.

---
+++ Photography/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Photography.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            return View();
+        }
+    }
+}
---
+++ Photography/Filter/SessionFilter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Photography.Filter
+{
+    public class SessionFilter
+    {
+        public static string UserName
+        {
+            get
+            {
+                if (HttpContext.Current.Session["UserName"] != null)
+                {
+                    return HttpContext.Current.Session["UserName"].ToString();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            set
+            {
+                HttpContext.Current.Session["UserName"] = value;
+            }
+        }
+        public class AuthorizationPrivilegeFilter : ActionFilterAttribute
+        {
+            public override void OnActionExecuting(ActionExecutingContext filterContext)
+            {
+                if (SessionFilter.UserName == null)
+                {
+                    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Login" }, { "action", "LogOut" } });
+                }
+                else
+                {
+                    base.OnActionExecuting(filterContext);
+                }
+            }
+        }
+
+    }
+}
